863c1c443cc412cdb3ced3029c3f088aeb01afad
Nancy <nan_alaobra@hotmail.com>
2022-10-19 02:36:44 -0300

    Conociendo el DOM

---
+++ script.js
@@ -0,0 +1,41 @@
+
+import checkComplete from "./component/checkComplete.js"
+import deleteIcon from "./component/deleteIcon.js"
+const btn = document.querySelector('[data-form-btn]');
+
+const createTask = (evento)=>{
+  evento.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value ="";
+
+  //Backticks
+  const taskContent = document.createElement("div")
+  taskContent.appendChild(checkComplete());
+  const titleTask = document.createElement("span")
+  titleTask.classList.add("task")
+  titleTask.innerText = value;
+  taskContent.appendChild(titleTask);
+  
+ 
+  //task.innerHTML = content;
+
+  task.appendChild(taskContent);
+  
+  task.appendChild(deleteIcon());
+
+  list.appendChild(task);
+};
+
+//Arrow funtions o funciones anónimas
+btn.addEventListener("click", createTask);
+
+
+
+
+
+
+
